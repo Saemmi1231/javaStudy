f7f42ba8dfc0eaed061c97381cf6db0b4715ca6e
Saemmi1231 <sammy3478@gmail.com>
2023-08-22 21:06:29 +0900

    overloading 공부

---
+++ ch1/Data.java
@@ -0,0 +1,22 @@
+class Data{
+    public static void main(String[]args){
+        Data1 d = new Data1();
+        Data2 d2 = new Data2();
+    }
+}
+
+class Data1 {
+    int value;
+    Data1(){};
+}
+
+class Data2{
+    int value;
+
+  Data2(){};
+    Data2(int x){
+        value = x;
+      
+    }
+}
+
